c4c7f88271a8f7a0b9a3013de99fe47852e93274
islaturrahman <Islhadevelopers@gmail.com>
2021-03-28 15:59:10 +0700

    initial comit awal

---
+++ javascript/script.js
@@ -0,0 +1,15 @@
+
+// menangkap semuu list materi
+const list = document.getElementsByTagName('li');
+const content = document.querySelector('#video-player');
+console.log(content);
+
+//membuat 
+
+
+
+function ubah(a){
+    content.innerHTML = html;
+}
+
+
